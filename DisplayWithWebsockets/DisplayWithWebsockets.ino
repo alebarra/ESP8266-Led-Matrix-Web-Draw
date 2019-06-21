c2012bae8c098a77108d6a972bd5c8da3f8a2a58
Alessandro Barracane 711567 <alessandro.barracane@edu.unito.it>
2019-06-21 17:17:57 +0200

    added index.html and js file to local storage, web server provide index.html file when clients connect to esp8266

--- DisplayWithWebsockets/DisplayWithWebsockets.ino
+++ DisplayWithWebsockets/DisplayWithWebsockets.ino
@@ -18,7 +18,14 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WiFiMulti.h> 
 ESP8266WiFiMulti wifiMulti; 
+ #include <FS.h>
+#include <ESP8266WebServer.h>
  
+ESP8266WebServer server(80);
+
+String getContentType(String filename); // convert the file extension to the MIME type
+bool handleFileRead(String path);       // send the right file to the client (if it exists)
+
 // ----------------------------
 // Standard Libraries - Already Installed if you have ESP8266 set up
 // ----------------------------
@@ -217,6 +224,60 @@ void clearDisplay(){
   }
 }
 
+
+String getContentType(String filename) { // convert the file extension to the MIME type
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  return "text/plain";
+}
+/*
+String getContentType(String filename){
+  if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+*/
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";         // If a folder is requested, send the index file
+  String contentType = getContentType(path);            // Get the MIME type
+  if (SPIFFS.exists(path)) {                            // If the file exists
+    File file = SPIFFS.open(path, "r");                 // Open it
+    size_t sent = server.streamFile(file, contentType); // And send it to the client
+    file.close();                                       // Then close the file again
+    return true;
+  }
+  Serial.println("\tFile Not Found");
+  return false;                                         // If the file doesn't exist, return false
+}
+
+void printDirContent()
+{
+  String str = "";
+  Dir dir = SPIFFS.openDir("/");
+  while (dir.next()) {
+      str += dir.fileName();
+      str += " / ";
+      str += dir.fileSize();
+      str += "\r\n";
+  }
+  Serial.print(str);
+ 
+}
+
+// SETUP
 void setup() {
 
   Serial.begin(115200);
@@ -285,11 +346,23 @@ void setup() {
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
   Serial.println("mDNS responder started, esp respond to "+localmDNSname+ ".local name");
+
+  SPIFFS.begin();   
+    server.onNotFound([]() {                              // If the client requests any URI
+    if (!handleFileRead(server.uri()))                  // send it if it exists
+      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+  });
+    server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
+
+  printDirContent();
+  
+  
 }
 
 void loop() {
   
   MDNS.update();
   webSocket.loop();
-
+  server.handleClient();
 }
